d52eb068eb33d40e2b01e7d12744d0aad662f1dd
Chad <chad@Chads-MacBook-Air.local>
2022-06-28 20:57:24 -0230

    First commit.

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import styled from 'styled-components/native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const Container = styled.View`
+    flex: 1;
+    background-color: #fff;
+    align-items: center;
+    justify-content: center;
+`;
+
+const Title = styled.Text`
+    background-color: green;
+    color: white;
+    font-size: 40px;
+    width: 80%;
+    text-align: center;
+`;
+
+const App = () => {
+    return (
+        <Container>
+            <Title>Brew Buddy</Title>
+            <StatusBar style="auto" />
+        </Container>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
